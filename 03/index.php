0f26ced000d836c44e614252d8bb76e70cd59c0f
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-06-24 12:49:35 -0300

    Add files via upload

---
+++ 03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e Constantes</title>
+</head>
+<body>
+    <h1>Variaveis</h1>
+
+    <?php 
+    $nome = "Henrique";
+
+    echo "Seu nome é $nome ?"; 
+     ?>
+</body>
+</html>
