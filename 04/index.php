c61e99f306fb953bdcc2573e7283f3d426326f1d
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-06-25 20:37:20 -0300

    Add files via upload

---
+++ 04/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Manipulação de Strings</h2>
+
+    <?php 
+    
+    $nome = "Henrique";
+    echo "Seu nome é  $nome";
+
+    const estado = "RJ";
+    echo " O estado em que você mora é ". estado;
+    ?>
+</body>
+</html>
